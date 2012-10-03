2165d930754571b15eaa0f9613338e595a820d54
SND\pvanbuijtene_cp <SND\pvanbuijtene_cp@7523448a-a8a0-4aac-83b0-0615d70583c7>
2012-10-03 18:18:56 +0000

    Added missing files for clearing current excluded items
    git-svn-id: https://scrumpowertools.svn.codeplex.com/svn/Main/ScrumPowerTools@16406 7523448a-a8a0-4aac-83b0-0615d70583c7

--- Packaging/MenuCommands.cs
+++ Packaging/MenuCommands.cs
@@ -13,7 +13,8 @@
         public const int CollapseAllReviewItems = 0x2007;
         public const int CompareWithPreviousVersion = 0x2008;
         public const int CompareWithVersionBeforeFirstChange = 0x2009;
-
+        public const int ClearExcludedItems = 0x2010;
+        
         public const int OpenTfsQuery1 = 0x3000;
         public const int OpenTfsQuery2 = 0x3001;
         public const int OpenTfsQuery3 = 0x3002;
